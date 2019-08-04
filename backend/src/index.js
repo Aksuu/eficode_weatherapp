c84af5c5743b29f2a6316cbb3aaae0606d396712
Aksu <appsu-@hotmail.com>
2019-08-05 00:39:11 +0300

    Fixing a stupid mistake

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-// Added my API key here
-const appId = process.env.APPID || '&APPID=599eac0e7024f3796ce2458aa42bc767';
+const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
